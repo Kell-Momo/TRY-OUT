efd997a334bc017decf519de351515971af6a1c9
momo <122658757+Kell-Momo@users.noreply.github.com>
2023-03-02 15:53:12 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+print('hello')
+i = 4
+d = 4.0
+s = 'HackerRank '
+# b*- Declare second integer, double, and String variables.
+a = int(input('enter a int: '))
+d1 = float(input("enter a float: "))
+s1 = str(input("enter a string"))
+
+# Read and save an integer, double, and String to your variables.
+result1 = i + a
+result2 = d1 + d
+
+# Print the sum of both integer variables on a new line.
+print(str(result1))
+
+# Print the sum of the double variables on a new line.
+print(str(result2))
+# Concatenate and print the String variables on a new line
+print(s+s1)
+# The 's' variable above should be printed first.
